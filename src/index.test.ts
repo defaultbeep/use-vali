2bd35af26805b4cf47ce6a0f1574eeb8cc4f87d5
Jonathan Goodman <jonathangoodman@hotmail.com>
2024-06-20 18:26:45 +0100

    feat: start useVali and tests

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,8 +1,136 @@
 import { describe, it, expect } from "@jest/globals";
-import { test } from "./index";
 
-describe("index", () => {
-  it("should pass", () => {
-    expect(test).toBe("test");
+import { z } from "zod";
+import { act, renderHook } from "@testing-library/react";
+import { useVali } from ".";
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+function mockEvent(name: string, value: string): any {
+  return {
+    target: {
+      name,
+      value,
+    },
+  };
+}
+
+describe("useForm", () => {
+  describe("initial state (with empty server state)", () => {
+    it("Should validate and set errors in state (Simple Form)", () => {
+      const serverState = {};
+      const schema = z.object({ name: z.string() });
+      const { result } = renderHook(() => useVali(schema, serverState));
+
+      /* The values are undefined so there will be 1 form level error */
+      expect(result.current.state.formErrors).toEqual(["Required"]);
+      expect(result.current.state.fieldErrors).toEqual({});
+
+      /* In this case values, touched and isSubmitted remain undefined */
+
+      expect(result.current.state.values).toBeUndefined();
+      expect(result.current.state.touched).toBeUndefined();
+      expect(result.current.state.isSubmitted).toBeUndefined();
+    });
+  });
+
+  describe("initial state (with server state)", () => {
+    describe("when server state has 'isSubmitted' as 'true'", () => {
+      it("Should return the given server state", () => {
+        const serverState = {
+          values: { name: "John" },
+          formErrors: [],
+          fieldErrors: {
+            name: { _errors: ["first name must include w"] },
+          },
+          isSubmitted: true,
+        };
+        const schema = z.object({
+          name: z
+            .string()
+            .includes("w", { message: "first name must include w" }),
+        });
+        const { result } = renderHook(() => useVali(schema, serverState));
+
+        /* The values are undefined so there will be 1 form level error */
+        expect(result.current.state.formErrors).toEqual([]);
+        expect(result.current.state.fieldErrors).toEqual({
+          name: { _errors: ["first name must include w"] },
+        });
+
+        /* In this case values, touched and isSubmitted are based on the server state */
+
+        expect(result.current.state.values).toEqual({ name: "John" });
+        expect(result.current.state.touched).toBeUndefined();
+        expect(result.current.state.isSubmitted).toBe(true);
+      });
+    });
+  });
+
+  describe("when user interacts with a field", () => {
+    it("Should update the value of a field in state while typing", () => {
+      const serverState = {};
+      const schema = z.object({ name: z.string() });
+      const { result } = renderHook(() => useVali(schema, serverState));
+
+      act(() => {
+        result.current.vali.onChange(mockEvent("name", "John"));
+      });
+
+      expect(result.current.state.values?.name).toEqual("John");
+    });
+
+    it("Should validate and set errors in state while typing", () => {
+      const serverState = {};
+      const schema = z.object({ name: z.string().min(5) });
+      const { result } = renderHook(() => useVali(schema, serverState));
+
+      act(() => {
+        result.current.vali.onChange(mockEvent("name", "John"));
+      });
+
+      expect(result.current.state.values?.name).toEqual("John");
+      expect(result.current.state.formErrors).toEqual([]);
+      expect(result.current.state.fieldErrors?.name?._errors).toEqual([
+        "String must contain at least 5 character(s)",
+      ]);
+    });
+
+    it("Should only mark a field as touched in state when the field is blurred", () => {
+      const serverState = {};
+      const schema = z.object({ name: z.string() });
+      const { result } = renderHook(() => useVali(schema, serverState));
+
+      expect(result.current.state.touched?.name).toBeUndefined();
+
+      act(() => {
+        result.current.vali.onChange(mockEvent("name", "John"));
+      });
+
+      expect(result.current.state.touched?.name).toBeUndefined();
+
+      act(() => {
+        result.current.vali.onBlur(mockEvent("name", "John"));
+      });
+
+      expect(result.current.state.touched?.name).toBe(true);
+    });
+
+    it("Should update the values of nested field names in state", () => {
+      const serverState = {};
+      const schema = z.object({
+        address: z.object({ line1: z.string(), line2: z.string() }),
+      });
+      const { result } = renderHook(() => useVali(schema, serverState));
+
+      act(() => {
+        result.current.vali.onChange(
+          mockEvent("address.line1", "1 Main Street"),
+        );
+      });
+
+      expect(result.current.state.values?.address?.line1).toEqual(
+        "1 Main Street",
+      );
+    });
   });
 });
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,108 @@
-export const test = "test";
+import { set } from "lodash";
+import { useEffect, useState } from "react";
+import { z } from "zod";
+
+type StuctureWith<T, TT> = {
+  [K in keyof T]?: T[K] extends string
+    ? TT
+    : T[K] extends Array<infer U>
+      ? Array<StuctureWith<U, TT>>
+      : T[K] extends object
+        ? StuctureWith<T[K], TT>
+        : never;
+};
+
+export type State<T> = {
+  values?: StuctureWith<T, string>;
+  touched?: StuctureWith<Required<T>, boolean>;
+  formErrors?: string[];
+  fieldErrors?: StuctureWith<Required<T>, { _errors: string[] }>;
+  isSubmitted?: boolean;
+};
+
+export function useVali<FV>(schema: z.Schema<FV>, serverState: State<FV>) {
+  const [state, setState] = useState<State<FV>>(serverState);
+
+  useEffect(() => {
+    if (serverState.isSubmitted) {
+      setState(serverState);
+    } else {
+      const result = schema.safeParse(state.values);
+      if (!result.success) {
+        setState({
+          ...state,
+          fieldErrors: { ...result.error.format(), _errors: undefined },
+          formErrors: result.error.format()._errors,
+        });
+      } else {
+        setState({
+          ...state,
+          fieldErrors: {},
+          formErrors: [],
+        });
+      }
+    }
+  }, [serverState, schema]);
+
+  const handleChange = (e: React.ChangeEvent<HTMLFormElement>) => {
+    const { name, value } = e.target;
+
+    const values = set({ ...state.values }, name, value);
+
+    const result = schema.safeParse(values);
+
+    if (!result.success) {
+      setState({
+        ...state,
+        values,
+        fieldErrors: { ...result.error.format(), _errors: undefined },
+        formErrors: result.error.format()._errors,
+      });
+    } else {
+      setState({
+        ...state,
+        values,
+        fieldErrors: {},
+        formErrors: [],
+      });
+    }
+  };
+
+  const handleBlur = (e: React.ChangeEvent<HTMLFormElement>) => {
+    const { name, value } = e.target;
+    if (name === "") return;
+
+    const values = set({ ...state.values }, name, value);
+
+    const touched = set({ ...state.touched }, name, true);
+
+    const result = schema.safeParse(values);
+
+    if (!result.success) {
+      const errors = result.error.format();
+      setState({
+        ...state,
+        values,
+        touched,
+        fieldErrors: { ...errors, _errors: undefined },
+        formErrors: errors._errors,
+      });
+    } else {
+      setState({
+        ...state,
+        values,
+        touched,
+        fieldErrors: {},
+        formErrors: [],
+      });
+    }
+  };
+
+  return {
+    state,
+    vali: {
+      onChange: handleChange,
+      onBlur: handleBlur,
+    },
+  };
+}

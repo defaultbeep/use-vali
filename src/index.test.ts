5b57da5f3d549705d11cb4aea2568697caf5ed8d
Jonathan Goodman <jonathangoodman@hotmail.com>
2024-06-20 13:03:19 +0100

    feat: add jest (inc coverage) with ts-jest

---
+++ src/index.test.ts
@@ -0,0 +1,8 @@
+import { describe, it, expect } from '@jest/globals';
+import { test } from './index';
+
+describe('index', () => {
+  it('should pass', () => {
+    expect(test).toBe('test');
+  });
+});
